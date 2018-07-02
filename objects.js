54d60afe799dd70ac94ca3b2d2aa84fe1c1d2486
kosatun@aol.com <kosatun@aol.com>
2018-07-02 01:22:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,9 +1,11 @@
-var playlist = new Object();
+var playlist = new Object({
+  artistName : songTitle
+});
   
 
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist[artistName] = songTitle;
    return playlist;
 }
 updatePlayList();
